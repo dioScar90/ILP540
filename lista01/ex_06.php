98a7c1bfe80ff32804f9d4e66e5dc1bc83d9da9c
Diogo L Scarmagnani <diogols@live.com>
2022-09-09 16:03:06 -0300

    ex_06 e readme

--- lista01/ex_06.php
+++ lista01/ex_06.php
@@ -46,8 +46,8 @@
     <p>IMC é a sigla para Índice de Massa Corpórea, parâmetro adotado pela Organização Mundial de Saúde para calcular o peso ideal de cada pessoa.</p>
     <p>O índice é calculado da seguinte maneira: divide-se o peso do paciente pela sua altura elevada ao quadrado. Diz-se que o indivíduo tem peso normal quando o resultado do IMC está entre 18,5 e 24,9.</p>
     <p>Quer descobrir seu IMC? Insira seu peso e sua altura nos campos abaixo e compare com os índices da tabela. Importante: siga os exemplos e use pontos como separadores.</p>
-
-    <form action="">
+	
+	<form action="">
         <div>
             <div class="inline">
                 <label class="cinza">Peso <span class="small">(ex.: 1,70)</span></label>
