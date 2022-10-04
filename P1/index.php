e04b9319764e13acaf0dc67b3284be2fc53b11db
Diogo L Scarmagnani <diogols@live.com>
2022-10-04 09:56:20 -0300

    starting P1

---
+++ P1/index.php
@@ -0,0 +1,13 @@
+<?php
+    include_once "header.php";
+    include_once "menu.php";
+?>
+
+<div class="container">
+    <h3> PHP </h3>
+    <p> Das ist meine erste Page. </p>
+</div>
+
+<?php
+    include_once "footer.php";
+?>
